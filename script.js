49162dec53e8002ff6f249d2e1bc2e91fdea3a9a
Anant Jain <Janant551@gmail.com>
2024-06-27 23:08:08 +0530

    Created the main page. What's left is to use weather API and make it responsive

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'a3b8ad06b1msh2d2538bc240fcf2p19c5fcjsn61116c92e8e0',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': 'a3b8ad06b1msh2d2538bc240fcf2p19c5fcjsn61116c92e8e0',
+// 		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+// const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-key': 'a3b8ad06b1msh2d2538bc240fcf2p19c5fcjsn61116c92e8e0',
+//         'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+//     }
+// };
+
+// async function fetchWeather() {
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json(); // Parse the JSON response
+//         console.log(result);
+//         document.getElementById('weather').innerText = JSON.stringify(result, null, 2);
+//     } catch (error) {
+//         console.error(error);
+//         document.getElementById('weather').innerText = 'Error fetching weather data.';
+//     }
+// }
+
+// fetchWeather();
