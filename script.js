645132935aaff2f162cbf1aebbe12dff97c5278d
Anant Jain <Janant551@gmail.com>
2024-07-03 21:54:40 +0530

    Created header and main area of the main webpage and implimented API, the website is not yet fully functional

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("searchcitybutton").addEventListener("click", function() {
-    window.location.href = "mainpage.html";
-  });
+const APIKey = "946d11b9f5340e4baa8967124ebc0fb8";
+const APIURL = "https://api.openweathermap.org/data/2.5/weather?q=Delhi";
+
+async function checkweather(){
+    const response = await fetch(APIURL + `&appid = ${APIKey}`);
+
+    var data = await response.json();
+    console.log(data);
+}
+
+checkweather();
